40d47f6853c2f3dfce50f63e860794738042bb26
ryotaroyama <ultrared3@yahoo.co.jp>
2021-03-18 14:06:12 +0900

    コントローラの処理をbefore_actionで共通化

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,10 +1,11 @@
 class FoodsController < ApplicationController
+  before_action :set_food, only: %i[show edit update destroy]
+
   def index
     @foods = Food.order(id: :asc)
   end
 
   def show
-    @food = Food.find(params[:id])
   end
 
   def new
@@ -17,22 +18,23 @@ class FoodsController < ApplicationController
   end
 
   def edit
-    @food = Food.find(params[:id])
   end
 
   def update
-    food = Food.find(params[:id])
-    food.update!(food_params)
+    @food.update!(food_params)
     redirect_to food
   end
 
   def destroy
-    food = Food.find(params[:id])
-    food.destroy!
+    @food.destroy!
     redirect_to root_path
   end
 
   private
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
 
   def food_params
     params.require(:food).permit(:name, :comment)
