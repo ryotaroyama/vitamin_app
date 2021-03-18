6d118ac2b5948d1ed936f2511f8ef9990487a978
ryotaroyama <ultrared3@yahoo.co.jp>
2021-03-18 10:35:17 +0900

    コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root ""
+  get 'foods/index'
+  get 'foods/show'
+  get 'foods/new'
+  get 'foods/create'
+  get 'foods/edit'
+  get 'foods/update'
+  get 'foods/destroy'
 end
